0657eb0ea92492dde2be8e3dd8cac111035d7373
piperal <piperal@tlu.ee>
2026-09-18 11:19:06 +0300

    chore: try to fix

--- Kodutöö2/task-tracker/vite.config.js
+++ Kodutöö2/task-tracker/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/piperal/kodt-d/tree/main/Kodut%C3%B6%C3%B62/task-tracker',
+  base: 'https://github.com/piperal/kodt-d/',
 });
