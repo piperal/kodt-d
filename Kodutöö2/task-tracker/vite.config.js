50deed30cd46d614ec62918ddac4f180208028dc
piperal <piperal@tlu.ee>
2026-09-18 11:29:49 +0300

    chore: change workflow

--- Kodutöö2/task-tracker/vite.config.js
+++ Kodutöö2/task-tracker/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/piperal/kodt-d/',
+  base: 'https://github.com/piperal/',
 });
